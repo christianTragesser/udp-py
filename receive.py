4a1b20de7f4d406c3cd5937604f4ada1eec4273a
Christian Tragesser <christian.tragesser@asynchrony.com>
2018-10-08 21:34:09 -0500

    timestamp for received message

--- receive.py
+++ receive.py
@@ -1,4 +1,5 @@
 import socket
+import datetime
  
 UDP_IP = "0.0.0.0"
 UDP_PORT = 5000
@@ -6,6 +7,9 @@ UDP_PORT = 5000
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
+start = datetime.datetime.now()
+print('{0} - INFO: UDP Receiver Started'.format(start))
 while True:
+    now = datetime.datetime.now() 
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print('Received message:', data.decode())
+    print('{0} Received message: {1:s}'.format(now, data.decode()))
